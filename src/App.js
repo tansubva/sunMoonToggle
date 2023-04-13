509b76d2304d7ad06bdb006727c1464401eb9600
Tansu Baktybaeva <tansubaktybaeva@Tansus-MacBook-Air.local>
2023-04-13 14:49:09 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import './App.css';
 function App() {
   const [isDaySwitch, setDaySwitch] = useState(false)
 
+  const handleDay = () => {
+    setDaySwitch(!isDaySwitch)
+  }
 
   return (
       <div>
         <div className="container">
-          <div className="sky">
-            <div className="sun"></div>
+          <div 
+          className="sky"
+          style={{backgroundColor: isDaySwitch ? 'blue' : 'black'}}
+          >
+            <div 
+            className="sun"
+            style={{backgroundColor: isDaySwitch ? 'yellow' : 'white'}}
+            ></div>
           </div>
+        </div>
+        <div className="container">
+        <button 
+        className="btn"
+        onClick={handleDay}
+        >
+          Switch</button>
+
         </div>
       </div>
   );
